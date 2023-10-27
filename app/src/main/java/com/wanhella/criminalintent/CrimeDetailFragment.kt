87ee4f4eea9133ece1f8f83e29d1a44a2e9ccad4
Stephen Wanhella <Stephen.wanhella@gmail.com>
2023-10-26 22:07:59 -0700

    Chapter 13 challenge

--- app/src/main/java/com/wanhella/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/wanhella/criminalintent/CrimeDetailFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.wanhella.criminalintent.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
@@ -27,6 +30,25 @@ class CrimeDetailFragment: Fragment() {
 
     private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
         CrimeDetailViewModelFactory(args.crimeId)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            crimeDetailViewModel.crime.value?.let {
+                if (it.title.isEmpty()) {
+                    handleOnBackCancelled()
+                    Toast.makeText(
+                        activity?.applicationContext,
+                        "Please enter a name for the crime",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    findNavController().popBackStack()
+                }
+            }
+        }
     }
 
     override fun onCreateView(
