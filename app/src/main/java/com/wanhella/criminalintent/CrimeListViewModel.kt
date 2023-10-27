3a1c0617a1a6a6f24e3d27a210831442463cc403
Stephen Wanhella <Stephen.wanhella@gmail.com>
2023-10-26 17:22:08 -0700

    Chapter 12 Coroutines

--- app/src/main/java/com/wanhella/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/wanhella/criminalintent/CrimeListViewModel.kt
@@ -1,13 +1,29 @@
 package com.wanhella.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
+private const val TAG = "CrimeListViewModel"
 class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
 
     init {
+        Log.d(TAG, "init starting")
+        viewModelScope.launch {
+            Log.d(TAG, "coroutine launched")
+            crimes += loadCrimes()
+            Log.d(TAG, "loading crimes finished")
+        }
+    }
+
+    suspend fun loadCrimes(): List<Crime> {
+        val result = mutableListOf<Crime>()
+        delay(5000L)
         for (i in 0 until 100) {
             val crime = Crime(
                 id = UUID.randomUUID(),
@@ -16,7 +32,9 @@ class CrimeListViewModel : ViewModel() {
                 isSolved = i % 2 == 0
             )
 
-            crimes += crime
+            result += crime
         }
+
+        return result
     }
 }
